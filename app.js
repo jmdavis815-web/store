8888020427670dd4d9a8bb95efca3e4a460d506a
jmdavis815-web <jmdavis815@gmail.com>
2025-11-23 02:06:51 -0600

    js added

--- app.js
+++ app.js
@@ -1,12 +1,65 @@
-function increaseQuantity(btn) {
-    let input = btn.parentNode.querySelector('input');
-    input.value = parseInt(input.value) + 1;
+let price = 25.99;
+let cart = 0;
+let cartTotal = 0;
+
+// Load saved info
+if (localStorage.getItem("cart")) {
+    cart = parseInt(localStorage.getItem("cart"));
+}
+if (localStorage.getItem("cartTotal")) {
+    cartTotal = parseInt(localStorage.getItem("cartTotal"));
 }
 
-function decreaseQuantity(btn) {
-    let input = btn.parentNode.querySelector('input');
-    let value = parseInt(input.value);
-    if (value > 1) {
-        input.value = value - 1;
+const cartItem = document.getElementById("cartItem");
+const priceTotal = document.getElementById("priceTotal");
+const cartBadge = document.getElementById("cartBadge");
+
+// Update UI when page loads
+function updateUI() {
+    cartItem.textContent = `${cart}`;
+    cartBadge.textContent = `${cartTotal}`;
+
+    let total = price * cart;
+    priceTotal.textContent = `Total: $${total.toFixed(2)}`;
+}
+updateUI();
+
+function saveCart() {
+    localStorage.setItem("cart", cart);
+    localStorage.setItem("cartTotal", cartTotal);
+}
+
+function removeCartTest() {
+    if (cart <= 0) {
+        cart = 0;
+        cartTotal = 0;
+    } else {
+        cart = cart - 1;
+        cartTotal = cartTotal - 1;
+    }
+
+    updateUI();
+    saveCart();
+}
+
+function addCartTest() {
+    if (cart >= 99) {
+        cart = 99;
+    } else {
+        cart = cart + 1;
+        cartTotal = cartTotal + 1;
+    }
+
+    updateUI();
+    saveCart();
+}
+
+function loadCartOnPage() {
+    let savedCartTotal = localStorage.getItem("cartTotal");
+
+    if (cartBadge && savedCartTotal !== null) {
+        cartBadge.textContent = savedCartTotal;
     }
 }
+
+loadCartOnPage();
